31d06e158c438b470f715424509a758ec3a616ca
clairehq <claireli.hq@gmail.com>
2020-04-24 20:42:52 -0700

    first commit

---
+++ precache-manifest.62f870c2d784953aad68de50ffe104ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57f543d1311f39bb0e2f5b9a4e6cca0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96bf31d381654388ea97",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "ba4fcd58bea787fab08e",
+    "url": "/static/css/main.88498902.chunk.css"
+  },
+  {
+    "revision": "96bf31d381654388ea97",
+    "url": "/static/js/2.6d23d5e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d23d5e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba4fcd58bea787fab08e",
+    "url": "/static/js/main.2dffe479.chunk.js"
+  },
+  {
+    "revision": "65b5bb304be9c8d41dc5",
+    "url": "/static/js/runtime-main.65820b94.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
